34eb1f7aa6016ad91d046a213f6b848176937597
Leo <leonardobrenis2312@gmail.com>
2025-09-06 18:43:01 -0500

    Subiendo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Subiendo a repositorio");
+        }
+    }
+}
